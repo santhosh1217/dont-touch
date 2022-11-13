c15244b947651064f93c3eff614e101ac58c2c53
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2022-11-13 09:32:24 +0530

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -13,20 +13,20 @@ class college(models.Model):
 class Student(models.Model):
     name = models.TextField()
     reg = models.TextField()
-    s_mobile = models.IntegerField(20)
-    p_mobile = models.IntegerField(20)
+    s_mobile = models.TextField()
+    p_mobile = models.TextField()
     attendance = models.BooleanField(default=False)
     clg = models.TextField()
     department = models.TextField()
     year = models.TextField()
 
 class Staff(models.Model):
-    staffName=models.TextField(max_length=50)
-    staffDep=models.TextField(max_length=40)
+    staffName=models.TextField()
+    staffDep=models.TextField()
     staffCollege = models.TextField()
-    staffPosition=models.TextField(max_length=20)
-    staffUsername=models.TextField(max_length=20)
-    staffPassword=models.TextField(max_length=20)
+    staffPosition=models.TextField()
+    staffUsername=models.TextField()
+    staffPassword=models.TextField()
 
 
 
