fb0915b1a1dd8e55088266d231f4c7d8d78cc9a7
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-11-19 08:07:14 -0800

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,6 +1,7 @@
 from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
+from PIL import Image
 
 
 # Create your models here.
@@ -10,7 +11,13 @@ class college(models.Model):
     password = models.TextField( )
     name=models.TextField()
     logo = models.ImageField( upload_to="pics")
-    
+    def save(self):
+        super().save()  # saving image first
+        img = Image.open(self.logo.path) # Open image using self
+        if img.height > 300 or img.width > 300:
+            new_img = (300, 300)
+            img.thumbnail(new_img)
+            img.save(self.logo.path)
 
 class Student(models.Model):
     name = models.TextField()
