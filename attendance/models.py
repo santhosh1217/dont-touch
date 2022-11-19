d0bea529bad5c8b67b9d41f8f25d9f7ac6acc8e9
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-11-19 09:23:01 -0800

    Update models.py

--- attendance/models.py
+++ attendance/models.py
@@ -1,10 +1,10 @@
 from distutils.command.upload import upload
 from email.policy import default
 from django.db import models
-from PIL import Image
-from io import BytesIO
-from django.core.files.uploadedfile import InMemoryUploadedFile
-import sys
+# from PIL import Image
+# from io import BytesIO
+# from django.core.files.uploadedfile import InMemoryUploadedFile
+# import sys
 
 
 # Create your models here.
@@ -14,14 +14,14 @@ class college(models.Model):
     password = models.TextField( )
     name=models.TextField()
     logo = models.ImageField( upload_to="pics")
-    def save(self, *args, **kwargs):
-        imageTemproary = Image.open(self.logo)
-        outputIoStream = BytesIO()
-        imageTemproaryResized = imageTemproary.resize( (1020,573) ) 
-        imageTemproaryResized.save(outputIoStream , format='JPEG', quality=85)
-        outputIoStream.seek(0)
-        self.logo = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" %self.logo.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
-        super(college, self).save(*args, **kwargs)
+#     def save(self, *args, **kwargs):
+#         imageTemproary = Image.open(self.logo)
+#         outputIoStream = BytesIO()
+#         imageTemproaryResized = imageTemproary.resize( (1020,573) ) 
+#         imageTemproaryResized.save(outputIoStream , format='JPEG', quality=85)
+#         outputIoStream.seek(0)
+#         self.logo = InMemoryUploadedFile(outputIoStream,'ImageField', "%s.jpg" %self.logo.name.split('.')[0], 'image/jpeg', sys.getsizeof(outputIoStream), None)
+#         super(college, self).save(*args, **kwargs)
 
 class Student(models.Model):
     name = models.TextField()
